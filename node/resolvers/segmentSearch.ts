2761f50db71c88c5541d24bf415b5011ed82605b
vichacin <victoria.chacin@vtex.com.br>
2022-06-13 17:12:11 -0300

    Add segment categories

--- node/resolvers/segmentSearch.ts
+++ node/resolvers/segmentSearch.ts
@@ -1,11 +1,16 @@
 export const queries = {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   searchSegment: async (_: unknown, args: SearchSegmentInput, __: Context) => {
-    // This is just a dummy implementation which returs the input itself
-    // const { myClient } = ctx.clients
-
-    console.log({ args })
-
-    return args.selectedFacets
-  },
+    const key = 'category-1'
+    if(args.isAuthenticated === true) {
+      return [
+        { key: key, value: '42' },
+        { key: key, value: '90' }
+      ]
+    }
+    return [
+      { key: key, value: '1' },
+      { key: key, value: '2' }
+    ]
+  }
 }
